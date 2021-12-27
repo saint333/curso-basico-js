d8b22f5c79cae197dda4d7d2d539c341103ec0d0
David Israel <israeldavi0904@gmail.com>
2021-12-26 19:04:34 -0500

    tipos de operadores

---
+++ operadores.js
@@ -0,0 +1,41 @@
+//operadores binarios
+
+3+2
+50-10
+10*20
+20/2
+
+
+//concatenacion
+"david"+"granados" //"davidgranados"
+
+//negacion
+!false // true
+
+//asignacion
+var a = 1 // = para asignar un valor
+
+//comparacion
+3 == "3" //compara el valor 3 no el tipo de cada uno
+
+5 < 3 // menor
+
+5 > 3 // mayor
+
+5 <= 6 //menor o igual
+
+5 >= 6 //mayor o igual
+
+a && b //si a y b son verdad las dos
+
+a || b //si a o b son verdad solo una o dos 
+
+//validacion
+3 === "3" //valida que sean del mismo tipo 
+
+//incrementacion
+
+var a = 40
+a++ // 40
+a++ //41
+a += 2 //43
